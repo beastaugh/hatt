1317683e304436fa168fb09bb823680fbbddb6ae
Benedict Eastaugh <benedict@eastaugh.net>
2012-12-03 14:31:03 +0000

    New parser which handles associativity and operator precedence.
    The exported interface (the function 'parseExpr') remains the same,
    but the entire implementation has been replaced by a new one based
    on Text.Parsec.Expr's expression parser generator.

--- src/Data/Logic/Propositional/Parser.hs
+++ src/Data/Logic/Propositional/Parser.hs
@@ -7,12 +7,9 @@ module Data.Logic.Propositional.Parser
 
 import Data.Logic.Propositional.Core (Expr (..), Var (..))
 
-import Text.ParserCombinators.Parsec
-    ((<|>), char, choice, eof, letter, parse, spaces, string, try)
-
-import Text.ParserCombinators.Parsec.Error (ParseError)
-import Text.ParserCombinators.Parsec.Pos (SourceName)
-import Text.ParserCombinators.Parsec.Prim (GenParser)
+import Data.Functor.Identity
+import Text.Parsec
+import Text.Parsec.Expr
 
 -- | The 'parseExpr' function accepts the name of a source, and a string to be
 -- parsed, and attempts to parse the string as a logical expression of the
@@ -36,47 +33,46 @@ import Text.ParserCombinators.Parsec.Prim (GenParser)
 -- need to be parenthesised. For example, @\"p -> (q & r)\"@ is a valid
 -- expression, although @\"(p -> (q & r))\"@ is also fine.
 parseExpr :: SourceName -> String -> Either ParseError Expr
-parseExpr = parse statement
+parseExpr = runP statement ()
 
-statement :: GenParser Char st Expr
+statement :: ParsecT String u Identity Expr
 statement = do spaces
-               x <- try binary <|> expr
+               x <- expr
                spaces
                eof
                return x
 
-expr :: GenParser Char st Expr
-expr = choice [exprP, negation, variable]
+expr :: ParsecT String u Identity Expr
+expr = buildExpressionParser operators term
+    <?> "compound expression"
 
-variable :: GenParser Char st Expr
+term :: ParsecT String u Identity Expr
+term =  parens expr
+    <|> variable
+    <?> "expression"
+
+variable :: ParsecT String u Identity Expr
 variable = do c <- letter
-              return $ Variable (Var c)
-
-negation :: GenParser Char st Expr
-negation = do char '~'
               spaces
-              x <- try expr <|> exprP
-              return $ Negation x
+              return $ Variable (Var c)
+        <?> "variable"
 
-exprP :: GenParser Char st Expr
-exprP = do char '('
-           spaces
-           x <- try binary <|> try expr <|> exprP
-           spaces
-           char ')'
-           return x
+parens :: ParsecT String u Identity Expr -> ParsecT String u Identity Expr
+parens p = do char '('
+              spaces
+              x <- p
+              char ')'
+              spaces
+              return x
+        <?> "parens"
 
-binary :: GenParser Char st Expr
-binary = do x1 <- expr
-            spaces
-            s  <- choice $ map string ["&", "|", "->", "<->"]
-            spaces
-            x2 <- try expr <|> exprP
-            return $ connective s x1 x2
-  where
-    connective c = case c of
-      "&"   -> Conjunction
-      "|"   -> Disjunction
-      "->"  -> Conditional
-      "<->" -> Biconditional
-      _     -> error "Impossible case"
+operators :: OperatorTable String u Identity Expr
+operators = [ [Prefix (string "~" >> return Negation)]
+            , [binary "&" Conjunction]
+            , [binary "|" Disjunction]
+            , [binary "->" Conditional]
+            , [binary "<->" Biconditional]
+            ]
+
+binary :: String -> (Expr -> Expr -> Expr) -> Operator String u Identity Expr
+binary n c = Infix (string n >> spaces >> return c) AssocRight
